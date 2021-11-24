fef973c5ce9b05a8f66c168e66bc2932fa92d71c
Joe Noël <joe.noel@focusrite.com>
2021-11-24 12:55:26 +0000

    Use peerjs for connection

--- src/Connection.jsx
+++ src/Connection.jsx
@@ -1,120 +1,60 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import Peer from 'peerjs';
+import {v4 as uuid} from 'uuid';
+
+const defaultId = uuid();
 
 export const Connection = () => {
-  const [localConnection, setLocalConnection] = useState(null);
-  const [remoteConnection, setRemoteConnection] = useState(null);
-  const [sendChannel, setSendChannel] = useState(null);
-  const [receiveChannel, setReceiveChannel] = useState(null);
-  const [isConnected, setConnected] = useState(false);
+  const [connection, setConnection] = useState(null);
+  const [peer, setPeer] = useState(null);
 
-  const handleSendChannelStatusChange = () => {
-    setConnected(sendChannel && sendChannel.readyState === 'open');
-  };
+  useEffect(() => {
+    const id = new URL(window.location).searchParams.get('id') || defaultId;
+    const peer = new Peer(id);
+    setPeer(peer);
 
-  const handleReceiveMessage = (event) => {
-    console.log(`Received ${event.data}`);
-  };
+    peer.on('connection', (conn) => {
+      conn.on('data', (data) => {
+        console.log(data);
+      });
 
-  const handleReceiveChannelStatusChange = () => {
-    if (receiveChannel) {
-      console.log(
-        "Receive channel's status has changed to " + receiveChannel.readyState
-      );
-    }
-  };
-
-  const receiveChannelCallback = (event) => {
-    const receiveChannel = event.channel;
-    receiveChannel.onmessage = handleReceiveMessage;
-    receiveChannel.onopen = handleReceiveChannelStatusChange;
-    receiveChannel.onclose = handleReceiveChannelStatusChange;
-    setReceiveChannel(receiveChannel);
-  };
-
-  const handleAddCandidateError = () => {
-    console.error('addICECandidate failed!');
-  };
-
-  const handleCreateDescriptionError = (error) => {
-    console.error('Unable to create an offer: ' + error.toString());
-  };
+      conn.on('open', () => {
+        console.log('Peer connected');
+      });
+    });
+  }, []);
 
   const connect = () => {
-    const localConnection = new RTCPeerConnection();
+    if (!peer) {
+      console.error('No peers :(');
+      return;
+    }
+    const connection = peer.connect('peak');
 
-    const sendChannel = localConnection.createDataChannel('sendChannel');
-    sendChannel.onopen = handleSendChannelStatusChange;
-    sendChannel.onclose = handleSendChannelStatusChange;
+    connection.on('open', () => {
+      console.log('Connected to peak');
+    });
 
-    const remoteConnection = new RTCPeerConnection();
-    remoteConnection.ondatachannel = receiveChannelCallback;
-
-    localConnection.onicecandidate = (e) =>
-      !e.candidate ||
-      remoteConnection
-        .addIceCandidate(e.candidate)
-        .catch(handleAddCandidateError);
-
-    remoteConnection.onicecandidate = (e) =>
-      !e.candidate ||
-      localConnection
-        .addIceCandidate(e.candidate)
-        .catch(handleAddCandidateError);
-
-    localConnection
-      .createOffer()
-      .then((offer) => localConnection.setLocalDescription(offer))
-      .then(() =>
-        remoteConnection.setRemoteDescription(localConnection.localDescription)
-      )
-      .then(() => remoteConnection.createAnswer())
-      .then((answer) => remoteConnection.setLocalDescription(answer))
-      .then(() =>
-        localConnection.setRemoteDescription(remoteConnection.localDescription)
-      )
-      .catch(handleCreateDescriptionError);
-
-    setLocalConnection(localConnection);
-    setRemoteConnection(remoteConnection);
-    setSendChannel(sendChannel);
+    setConnection(connection);
   };
 
-  const disconnect = () => {
-    if (sendChannel) {
-      sendChannel.close();
+  const disconnect = () => {};
+
+  const send = () => {
+    if (connection) {
+      connection.send('HI!');
     }
-
-    if (receiveChannel) {
-      receiveChannel.close();
-    }
-
-    if (localConnection) {
-      localConnection.close();
-    }
-
-    if (remoteConnection) {
-      remoteConnection.close();
-    }
-
-    setSendChannel(null);
-    setReceiveChannel(null);
-    setLocalConnection(null);
-    setRemoteConnection(null);
-  };
-
-  const sendMidi = () => {
-    sendChannel.send('Hello?');
   };
 
   return (
     <div>
-      <button onClick={connect} disabled={isConnected}>
+      <button onClick={connect} disabled={connection}>
         Connect
       </button>
-      <button onClick={disconnect} disabled={!isConnected}>
+      <button onClick={disconnect} disabled={!connection}>
         Disconnect
       </button>
-      <button onClick={sendMidi} disabled={!isConnected}>
+      <button onClick={send} disabled={!connection}>
         Send MIDI to peer
       </button>
     </div>
