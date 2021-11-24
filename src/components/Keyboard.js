c0c199f876bfba02314f7e49030f8bff68cdf714
laramackey <lara.mackey@focusrite.com>
2021-11-24 17:01:54 +0000

    Add ability to change octave

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Keyboard.css';
 import Key from './Key'
 
 const Keyboard = (props) => {
-
+  const [noteChange, setOctave] = useState(0);
   const sendNoteEvent = (isNoteOn, note) => {
     const statusByte = isNoteOn ? 0x90 : 0x80;
-    const midiMessage = [statusByte, note, 0x40]
+    const midiMessage = [statusByte, note + noteChange, 0x40]
     const destination = props.device && props?.connection?.id === 'peak-6837fff9-8481-4046-992a-235da95f01c8' ? props.device : props?.connection?.send ? props.connection : null;
     if (destination) {
       destination.send(midiMessage);
     } 
+  }
+  const setNoteChange = (diff)=> {
+    setOctave(noteChange + diff)
   }
   const setNote = (note) => {
     sendNoteEvent(true, note)
@@ -20,7 +23,7 @@ const Keyboard = (props) => {
   };
   return (
     <div className="keyboard" id="keyboard">
-      <button style={{width: '50px', height: '50px', marginRight: '20px'}}>&lt;</button>
+      <button onClick={() => setNoteChange(-12)} style={{width: '50px', height: '50px', marginRight: '20px'}}>&lt;</button>
       <Key className='whitekey' setNote={setNote}  stopNote={stopNote} note={72} />
       <Key className='blackkey' setNote={setNote}  stopNote={stopNote} note={73} />
       <Key className='whitekey' setNote={setNote}  stopNote={stopNote} note={74} />
@@ -39,7 +42,7 @@ const Keyboard = (props) => {
       <Key className='blackkey' setNote={setNote}  stopNote={stopNote} note={87} />
       <Key className='whitekey' setNote={setNote}  stopNote={stopNote} note={88} />
       <Key className='whitekey' setNote={setNote}  stopNote={stopNote} note={89} />
-      <button style={{width: '50px', height: '50px', marginLeft: '20px'}}>&gt;</button>
+      <button onClick={() => setNoteChange(+12)} style={{width: '50px', height: '50px', marginLeft: '20px'}}>&gt;</button>
     </div>
   );
 };
